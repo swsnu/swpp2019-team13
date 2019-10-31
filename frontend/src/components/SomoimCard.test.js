63cbb485487ae85d4733d3f75d70b1a35208b3ea
jihoonrf <kzc123@rideflux.com>
2019-10-31 13:17:18 +0900

    somoim card test done

---
+++ frontend/src/components/SomoimCard.test.js
@@ -0,0 +1,117 @@
+import React from "react";
+import { mount, shallow } from "enzyme";
+import { Provider } from "react-redux";
+import { ConnectedRouter } from "connected-react-router";
+import { Route, Switch } from "react-router-dom";
+
+import SomoimCard from "./SomoimCard";
+import { getMockStore } from "../test-utils/mocks";
+import { history } from "../store/store";
+
+const stubInitialState = {
+  tags: [
+    { id: 0, name: "friendship" },
+    { id: 1, name: "love" },
+    { id: 2, name: "sport" },
+    { id: 3, name: "game" },
+    { id: 4, name: "study" },
+    { id: 5, name: "music" },
+    { id: 6, name: "art" },
+    { id: 7, name: "nothing" }
+  ],
+
+  somoims: [
+    {
+      id: 0,
+      title: "Let's LoL!",
+      summary: "Playing LoL together!! :D",
+      description: "LoL\nLoL\nLol",
+      selected_dept: [0, 1],
+      available_sem: 1,
+      tag: [0, 1],
+      goalJoiner: 20,
+      currentJoiner: 7,
+      likes: 10
+    },
+    {
+      id: 1,
+      title: "Book lovers",
+      summary: "We read books until we fall asleep..",
+      description: "Actually, it's sleep somoim :)",
+      selected_dept: [0, 1, 3, 4, 5],
+      available_sem: 1,
+      tag: [2, 3],
+      goalJoiner: 10,
+      currentJoiner: 3,
+      likes: 5
+    },
+    {
+      id: 2,
+      title: "test somoim",
+      summary: "i am testing the somoim list",
+      description: "Me too bro",
+      selected_dept: [0, 1, 3, 4, 5],
+      available_sem: 3,
+      tag: [4, 5],
+      goalJoiner: 10,
+      currentJoiner: 9,
+      likes: 5
+    },
+    {
+      id: 3,
+      title: "301 assa somoim",
+      summary: "We are assa in 301",
+      description: "Sad..",
+      selected_dept: [0, 1, 3, 4, 5],
+      available_sem: 5,
+      tag: [6, 7],
+      goalJoiner: 10,
+      currentJoiner: 1,
+      likes: 5
+    }
+  ]
+};
+
+const mockStore = getMockStore(stubInitialState);
+
+describe("<SomoimCard />", () => {
+  let somoimCard;
+  let spyClickHandler;
+  beforeEach(() => {
+    somoimCard = (
+      <Provider store={mockStore}>
+        <ConnectedRouter history={history}>
+          <Switch>
+            <Route
+              path="/"
+              exact
+              render={() => {
+                return (
+                  <SomoimCard
+                    somoim={stubInitialState.somoims[0]}
+                    clickHandler={spyClickHandler}
+                  />
+                );
+              }}
+            />
+          </Switch>
+        </ConnectedRouter>
+      </Provider>
+    );
+
+    spyClickHandler = jest.fn();
+  });
+
+  it("should render without errors", () => {
+    const component = mount(somoimCard);
+    const wrapper = component.find("SomoimCard");
+    expect(wrapper.length).toBe(1);
+  });
+
+  it("should handle clicks", () => {
+    const component = mount(somoimCard);
+    const wrapper = component.find("Card");
+    wrapper.simulate("click");
+    expect(spyClickHandler).toHaveBeenCalledTimes(1);
+  });
+});
