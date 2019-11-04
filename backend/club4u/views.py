cb48deb24d8281a512d5039b1cc6a7adb1439e6e
jihoonrf <kzc123@rideflux.com>
2019-11-04 17:22:13 +0900

    model relationship revised

--- backend/club4u/views.py
+++ backend/club4u/views.py
@@ -3,11 +3,13 @@ from django.contrib.auth.models import User
 from django.views.decorators.csrf import ensure_csrf_cookie
 import json
 from json import JSONDecodeError
-from .models import UserProfile, PreClub, Club, Somoim, Tag, Department, Category, Major, UserLikeClub, UserApplyClub, UserLikeSomoim, UserJoinSomoim
+from .models import UserProfile, PreClub, Club, Somoim, Tag, Department, Category, Major
 from django.contrib.auth import login, authenticate, logout
 from django.core.exceptions import ObjectDoesNotExist
 
 # api/major/list/
+
+
 def major_list(request):
     if request.method == 'GET':
         response_dict = [major for major in Major.objects.all().values()]
@@ -16,6 +18,8 @@ def major_list(request):
         return HttpResponse(status=405)
 
 # api/dept/list/
+
+
 def dept_list(request):
     if request.method == 'GET':
         response_dict = [dept for dept in Department.objects.all().values()]
@@ -24,6 +28,8 @@ def dept_list(request):
         return HttpResponse(status=405)
 
 # api/user/signup/
+
+
 def signup(request):
     if request.method == 'POST':
         req_data = json.loads(request.body.decode())
@@ -35,13 +41,16 @@ def signup(request):
         available_semester = req_data['available_semester']
         user = User.objects.create_user(username=username, password=password)
         user.save()
-        userprofile = UserProfile(user=user, dept=dept, major=major, grade=grade, available_semester=available_semester)
+        userprofile = UserProfile(
+            user=user, dept=dept, major=major, grade=grade, available_semester=available_semester)
         userprofile.save()
         return HttpResponse(status=201)
     else:
         return HttpResponse(status=405)
 
 # api/club/list/
+
+
 def club_list(request):
     if request.method == 'GET':
         response_dict = [club for club in Club.objects.all().values()]
@@ -61,14 +70,17 @@ def somoim_list(request):
             article_title = json.loads(body)['title']
             article_content = json.loads(body)['content']
             acc_user = User.objects.get(username=request.user)
-            article = Article(title=article_title, content=article_content, author=acc_user)
+            article = Article(title=article_title,
+                              content=article_content, author=acc_user)
             article.save()
-            response_dict = {'id' : article.id, 'title' : article.title, 'content' : article.content, 'author' : article.author_id}
+            response_dict = {'id': article.id, 'title': article.title,
+                             'content': article.content, 'author': article.author_id}
             return JsonResponse(response_dict, status=201)
         except (KeyError, JSONDecodeError):
             return HttpResponse(status=400)
     else:
         return HttpResponse(status=405)
+
 
 @ensure_csrf_cookie
 def token(request):
