1f42a3cc057ff4c30c8c451f60644defffae0281
spiraline <spiraline@naver.com>
2019-11-05 22:15:34 +0900

    Login Fail Message doesn't show when login success

--- frontend/src/store/actions/user.js
+++ frontend/src/store/actions/user.js
@@ -22,12 +22,9 @@ export const signIn_ = user => {
 
 export const signIn = loginInfo => {
   return dispatch => {
-    return (
-      axios
-        .post("/api/user/signin/", loginInfo)
-        // .catch(e => "fail")
-        .then(res => dispatch(signIn_(res.data)))
-    );
+    return axios
+      .post("/api/user/signin/", loginInfo)
+      .then(res => dispatch(signIn_(res.data)));
   };
 };
 
