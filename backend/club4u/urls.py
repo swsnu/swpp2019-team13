5aeb3ec6c01e6c9672c49cb06c42dd46199686c5
jihoonrf <kzc123@rideflux.com>
2019-11-09 02:13:46 +0900

    backend code arrangement

--- backend/club4u/urls.py
+++ backend/club4u/urls.py
@@ -3,26 +3,24 @@ from club4u import views
 
 urlpatterns = [
     path('token/', views.token, name='token'),
-
-    path('major/list/', views.major_list, name='major_list'),
-    path('dept/list/', views.dept_list, name='dept_list'),
-    path('club/list/', views.club_list, name='club_list'),
-    path('somoim/list/', views.somoim_list, name='somoim_list'),
+    path('preclub/', views.pre_club, name='pre_club'),
 
     path('category/list/', views.category_list, name='category_list'),
     path('tag/list/', views.tag_list, name='tag_list'),
-    path('preclub/', views.pre_club, name='pre_club'),
+    path('dept/list/', views.dept_list, name='dept_list'),
+    path('major/list/', views.major_list, name='major_list'),
 
-    path('club/edit/<int:id>/', views.club_edit, name='club_edit'),
-    path('somoim/edit/<int:id>/', views.somoim_edit, name='somoim_edit'),
+    path('user/list/', views.user_list, name='user_list'),
+
 
     path('user/signin/', views.signin, name='signin'),
     path('user/signup/', views.signup, name='signup'),
     path('user/signout/', views.signout, name='signout'),
-    path('user/list/', views.user_list, name='user_list'),
     path('user/logininfo/', views.logininfo, name='logininfo'),
-
     path('user/information/', views.information, name='information'),
+
+    path('club/list/', views.club_list, name='club_list'),
+    path('somoim/list/', views.somoim_list, name='somoim_list'),
 
     path('user/<int:id>/club/manage/', views.manage_club, name='manage_club'),
     path('user/<int:id>/club/like/', views.like_club, name='like_club'),
