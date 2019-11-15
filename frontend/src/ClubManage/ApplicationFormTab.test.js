189ff7b186a10977f31c1e5d1030e8108825120c
jihoonrf <kzc123@rideflux.com>
2019-11-16 03:48:01 +0900

    application form tab test done

--- frontend/src/ClubManage/ApplicationFormTab.test.js
+++ frontend/src/ClubManage/ApplicationFormTab.test.js
@@ -25,9 +25,16 @@ let stubInitialState = {
   applicationForm: {
     short_texts: [{ title: "short_title", order: 0 }],
     long_texts: [{ title: "long_title", order: 1 }],
-    multi_choices: [{ title: "long_title", order: 2, choices: [] }],
-    images: [{ title: "image_title", order: 3 }],
-    files: [{ title: "file_title", order: 4 }]
+    multi_choices: [
+      {
+        title: "multi_title",
+        order: 2,
+        choices: [{ content: "a", isDeleted: false, id: 0 }],
+        choiceID: 1
+      }
+    ],
+    images: [{ title: "image_title", order: 4 }],
+    files: [{ title: "file_title", order: 3 }]
   }
 };
 
@@ -76,55 +83,181 @@ describe("<ApplicationFormTab />", () => {
     let component = mount(applicationFormTab);
     let wrapper = component.find("ApplicationFormTab");
     let mainInstance = component.find("ApplicationFormTab").instance();
-    expect(wrapper.length).toBe(1);
 
-    mainInstance.setState({ ...component.state, forceRender: Math.random() });
+    mainInstance.setState({
+      ...mainInstance.state,
+      forceRender: Math.random()
+    });
+    expect(wrapper.length).toBe(1);
   });
 
-  //   it("list item click handle", () => {
-  //     let component = mount(applicationFormTab);
-  //     let mainInstance = component.find("ApplicationFormTab").instance();
-  //     let wrapper = component.find("h1");
-  //     wrapper.at(0).simulate("click");
-  //     wrapper = component.find("#list-item-body");
-  //     wrapper.at(1).simulate("click");
-  //     expect(mainInstance.state.clubDetailShow).toBe(true);
+  it("short form add button click handle", () => {
+    let component = mount(applicationFormTab);
+    let mainInstance = component.find("ApplicationFormTab").instance();
+    let wrapper = component.find(".short-form-button");
+    let id = mainInstance.state.formID;
+    wrapper.at(0).simulate("click");
+    expect(mainInstance.state.formID).toBe(id + 1);
+  });
+  it("long form add button click handle", () => {
+    let component = mount(applicationFormTab);
+    let mainInstance = component.find("ApplicationFormTab").instance();
+    let wrapper = component.find(".long-form-button");
+    let id = mainInstance.state.formID;
+    wrapper.at(0).simulate("click");
+    expect(mainInstance.state.formID).toBe(id + 1);
+  });
+  it("multi choice form add button click handle", () => {
+    let component = mount(applicationFormTab);
+    let mainInstance = component.find("ApplicationFormTab").instance();
+    let wrapper = component.find(".multi-form-button");
+    let id = mainInstance.state.formID;
+    wrapper.at(0).simulate("click");
+    expect(mainInstance.state.formID).toBe(id + 1);
+  });
+  it("image form add button click handle", () => {
+    let component = mount(applicationFormTab);
+    let mainInstance = component.find("ApplicationFormTab").instance();
+    let wrapper = component.find(".image-form-button");
+    let id = mainInstance.state.formID;
+    wrapper.at(0).simulate("click");
+    expect(mainInstance.state.formID).toBe(id + 1);
+  });
+  it("file form add button click handle", () => {
+    let component = mount(applicationFormTab);
+    let mainInstance = component.find("ApplicationFormTab").instance();
+    let wrapper = component.find(".file-form-button");
+    let id = mainInstance.state.formID;
+    wrapper.at(0).simulate("click");
+    expect(mainInstance.state.formID).toBe(id + 1);
+  });
 
-  //     wrapper = component.find("#spyClubDetail");
-  //     wrapper.simulate("click");
-  //     expect(mainInstance.state.clubDetailShow).toBe(false);
-  //   });
+  it("save button click handle", () => {
+    let component = mount(applicationFormTab);
+    let mainInstance = component.find("ApplicationFormTab").instance();
+    let wrapper = component.find(".form-save-button");
+    mainInstance.setState({
+      ...mainInstance.state,
+      forceRender: Math.random()
+    });
+    wrapper.at(0).simulate("click");
+    expect(spyPutApplicationFormByID).toBeCalledTimes(1);
+  });
 
-  //   it("unlike button click handle", () => {
-  //     let component = mount(applicationFormTab);
-  //     let mainInstance = component.find("ApplicationFormTab").instance();
-  //     let wrapper = component.find("#club-unlike-button");
-  //     wrapper.at(1).simulate("click");
-  //     expect(spyAddLikedClub).toBeCalledTimes(1);
-  //   });
+  it("choice add/remove click handle", () => {
+    let component = mount(applicationFormTab);
+    let mainInstance = component.find("ApplicationFormTab").instance();
+    mainInstance.setState({
+      ...mainInstance.state,
+      forceRender: Math.random()
+    });
+    component.update();
+    let wrapper = component.find(".choice-add-button");
+    wrapper.at(0).simulate("click");
+    expect(mainInstance.state.formList[2].choices.length).toBe(2);
+    wrapper.at(0).simulate("click");
+    wrapper = component.find(".choice-remove-button");
+    wrapper.at(0).simulate("click");
+    expect(mainInstance.state.formList[2].choices[0].isDeleted).toBe(true);
+  });
 
-  //   it("when likedClubs info is not loaded", () => {
-  //     let savedInfo = stubInitialState.likedClubs;
-  //     stubInitialState.likedClubs = null;
-  //     mockStore = getMockStore(stubInitialState);
-  //     applicationFormTab = (
-  //       <Provider store={mockStore}>
-  //         <ConnectedRouter history={history}>
-  //           <Switch>
-  //             <Route
-  //               path="/"
-  //               exact
-  //               render={() => {
-  //                 return <ApplicationFormTab />;
-  //               }}
-  //             />
-  //           </Switch>
-  //         </ConnectedRouter>
-  //       </Provider>
-  //     );
-  //     const component = mount(applicationFormTab);
+  it("multi choice default onChange handle", () => {
+    let component = mount(applicationFormTab);
+    let mainInstance = component.find("ApplicationFormTab").instance();
+    mainInstance.setState({
+      ...mainInstance.state,
+      forceRender: Math.random()
+    });
+    component.update();
+    let wrapper = component.find(".multi-default-choice");
+    wrapper.at(0).simulate("change", { target: { value: "aa" } });
+    expect(mainInstance.state.formList[2].defaultChoice).toEqual("aa");
+  });
 
-  //     stubInitialState.likedClubs = savedInfo;
-  //     mockStore = getMockStore(stubInitialState);
-  //   });
+  it("multi choice onChange handle", () => {
+    let component = mount(applicationFormTab);
+    let mainInstance = component.find("ApplicationFormTab").instance();
+    mainInstance.setState({
+      ...mainInstance.state,
+      forceRender: Math.random()
+    });
+    component.update();
+    let wrapper = component.find(".choice-add-button");
+    wrapper.at(0).simulate("click");
+    wrapper.at(0).simulate("click");
+    wrapper = component.find(".multi-choice-textbox");
+    wrapper.at(0).simulate("change", { target: { value: "aa" } });
+    expect(mainInstance.state.formList[2].choices[0].content).toEqual("aa");
+  });
+
+  it("form delete button click handle", () => {
+    let component = mount(applicationFormTab);
+    let mainInstance = component.find("ApplicationFormTab").instance();
+    mainInstance.setState({
+      ...mainInstance.state,
+      forceRender: Math.random()
+    });
+    component.update();
+    let wrapper = component.find(".form-delete-button");
+    wrapper.at(0).simulate("click");
+    expect(mainInstance.state.formList[0].isDeleted).toEqual(true);
+  });
+
+  it("form title input onChange handle", () => {
+    let component = mount(applicationFormTab);
+    let mainInstance = component.find("ApplicationFormTab").instance();
+    mainInstance.setState({
+      ...mainInstance.state,
+      forceRender: Math.random()
+    });
+    component.update();
+    let wrapper = component.find(".form-title-input");
+    wrapper.at(0).simulate("change", { target: { value: "aa" } });
+    expect(mainInstance.state.formList[0].title).toEqual("aa");
+  });
+
+  it("non-existing form case (cannot happen)", () => {
+    let component = mount(applicationFormTab);
+    let wrapper = component.find("ApplicationFormTab");
+    let mainInstance = component.find("ApplicationFormTab").instance();
+
+    mainInstance.setState({
+      ...mainInstance.state,
+      formList: mainInstance.state.formList.concat({ type: "", id: 100 })
+    });
+    expect(wrapper.length).toBe(1);
+  });
+
+  it("form data is not prepared (cannot happen)", () => {
+    let saved = stubInitialState.applicationForm;
+    stubInitialState.applicationForm = null;
+    mockStore = getMockStore(stubInitialState);
+    applicationFormTab = (
+      <Provider store={mockStore}>
+        <ConnectedRouter history={history}>
+          <Switch>
+            <Route
+              path="/"
+              exact
+              render={() => {
+                return <ApplicationFormTab />;
+              }}
+            />
+          </Switch>
+        </ConnectedRouter>
+      </Provider>
+    );
+    let component = mount(applicationFormTab);
+    let wrapper = component.find("ApplicationFormTab");
+    let mainInstance = component.find("ApplicationFormTab").instance();
+
+    mainInstance.setState({
+      ...mainInstance.state,
+      formList: mainInstance.state.formList.concat({ type: "", id: 100 })
+    });
+    expect(wrapper.length).toBe(1);
+
+    stubInitialState.applicationForm = saved;
+    mockStore = getMockStore(stubInitialState);
+  });
 });
