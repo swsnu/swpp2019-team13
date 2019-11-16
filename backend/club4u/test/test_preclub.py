7389673d8713851ec68e456156706abd750673f4
spiraline <spiraline@naver.com>
2019-11-16 13:50:40 +0900

    Achieve 99% coverage in backend

--- backend/club4u/test/test_preclub.py
+++ backend/club4u/test/test_preclub.py
@@ -1,21 +1,8 @@
-# import json
+import json
 from django.test import TestCase, Client
-from ..models import User, UserProfile, Department, Category, Major
-
-# for test with file
-# from django.test import override_settings
-# from django.core.files.uploadedfile import SimpleUploadedFile
-# import base64
-# import os
-
-# small_gif = (
-#     b'\x47\x49\x46\x38\x39\x61\x01\x00\x01\x00\x00\x00\x00\x21\xf9\x04'
-#     b'\x01\x0a\x00\x01\x00\x2c\x00\x00\x00\x00\x01\x00\x01\x00\x00\x02'
-#     b'\x02\x4c\x01\x00\x3b'
-# )
+from ..models import User, UserProfile, Department, Category, Major, PreClub
 
 
-# @override_settings(MEDIA_ROOT='club4u/test/testdata/')
 class PreClubTestCase(TestCase):
     def setUp(self):
         Category.objects.create(id=1, name='category1')
@@ -25,20 +12,15 @@ class PreClubTestCase(TestCase):
             id=1, username='user1', password='pw1', last_name='name1')
         UserProfile.objects.create(id=1, user=user, dept=dept, major=major)
 
-    # def test_post_preclub_success(self):
-    #     client = Client(enforce_csrf_checks=False)
+    def test_post_preclub_success(self):
+        client = Client(enforce_csrf_checks=False)
+        test_json = json.dumps(
+            {'name': 'name', 'manager': 'manager', 'category': 1, 'auth_img': {}})
+        response = client.post('/api/preclub/list/',
+                               test_json, content_type='application/json')
 
-    #     # mock_img = SimpleUploadedFile(
-    #     #     'test.gif', small_gif, content_type='image/gif')
-
-    #     with open(os.path.join('club4u/test/testdata/', "test.jpg")) as file:
-    #         response = client.post('/api/preclub/list/', json.dumps(
-    #             {'name': 'user1', 'category': 1, 'manager': 'pw1',
-    #              'auth_img': }),
-    #             content_type='application/json')
-
-    #     self.assertEqual(response.status_code, 201)
-    #     self.assertEqual(len(PreClub.objects.all()), 1)
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(len(PreClub.objects.all()), 1)
 
     def test_preclub_wrong_method(self):
         client = Client(enforce_csrf_checks=False)
