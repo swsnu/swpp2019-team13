8754599c50e0e7b1f62b94b39aa28c3cc40ec1e3
jihoonrf <kzc123@rideflux.com>
2019-11-25 16:16:36 +0900

    load test with club, somoim, and other additional infos

--- backend/load_test.py
+++ backend/load_test.py
@@ -9,7 +9,17 @@ class LoadTask(TaskSet):
         pass
 
     @task
+    def load_additional_info(self):
+        self.client.get("/api/tag/list/")
+        self.client.get("/api/dept/list/")
+        self.client.get("/api/major/list/")
+
+    @task
     def club_list(self):
+        self.client.get("/api/club/list/")
+
+    @task
+    def somoim_list(self):
         self.client.get("/api/club/list/")
 
 
