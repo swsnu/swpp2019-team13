216c4645d19ecfb4f2e857695cdf8c06b82ff40d
spiraline <spiraline@naver.com>
2019-11-27 16:19:35 +0900

    Remove print while extract tag

--- backend/club4u/views.py
+++ backend/club4u/views.py
@@ -53,7 +53,6 @@ def tag_extlist(request):
         for i in range(0, 10):
             if len(word) == i:
                 break
-            print(word[i])
             # drop tag if it exists in list and selected ratio is lower than 0.5
             if Tag.objects.filter(name=word[i]).exists():
                 tag = Tag.objects.get(name=word[i])
