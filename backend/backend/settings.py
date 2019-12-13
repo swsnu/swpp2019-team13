014c1f82804f0f744dd85fc1e98d15f96359495d
spiraline <spiraline@naver.com>
2019-12-13 11:57:54 +0000

    setting fixed

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = 'k8a)0uv397k(5n9^kllqucj5c@^@9)&t&u3a@w(wlw2%ndvk#1'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['${52.141.23.207}', '${club4u.xyz}', 'localhost', '127.0.0.1']
+# ALLOWED_HOSTS = ['${52.141.23.207}', '${club4u.xyz}', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
